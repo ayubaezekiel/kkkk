4084caacb34333142dc7c2948282816a32a5df83
ayubaezekiel <ezekielayoba@yahoo.com>
2025-02-13 18:56:37 +0100

    add development callout to dashboard route; integrate Radix UI components for user notification

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,41 @@
 import Dashboard from "@/components/dashboard";
+import { Callout, Heading, IconButton, Text } from "@radix-ui/themes";
 import { createFileRoute } from "@tanstack/react-router";
+import { TriangleAlert } from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <Dashboard />;
+  return (
+    <>
+      <div className="fixed inset-0 bg-black/80 bg-opacity-50 z-40" />
+      <Callout.Root
+        color="blue"
+        style={{
+          position: "fixed",
+          inset: 0,
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "auto",
+        }}
+        className="z-50 flex max-w-md max-h-96"
+      >
+        <Callout.Icon>
+          <IconButton color="blue" variant="ghost" radius="full">
+            <TriangleAlert />
+          </IconButton>
+        </Callout.Icon>
+        <Callout.Text color="blue">
+          <Heading className="mb-4">Under Development</Heading>
+          <Text>
+            This page is currently under development. Please check back later or
+            stay tuned with any of our socials.
+          </Text>
+        </Callout.Text>
+      </Callout.Root>
+      <Dashboard />
+    </>
+  );
 }
